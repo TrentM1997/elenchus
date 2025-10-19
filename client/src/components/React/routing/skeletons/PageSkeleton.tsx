75349d688cd15507c8e6e90b41183778aca0fe24
Trent Irvin <t.m.irvin1997@gmail.com>
2025-10-19 13:51:59 -0500

    basehead.astro patch attempt for link preview

--- client/src/components/React/routing/skeletons/PageSkeleton.tsx
+++ client/src/components/React/routing/skeletons/PageSkeleton.tsx
@@ -1,7 +1,6 @@
 import DisplayLoader from "../../Shared/Loaders/DisplayLoader";
 import AppWindowIcon from "../../Shared/IconComponents/AppWindowIcon";
 
-
 export default function Pageskeleton() {
 
     return (
