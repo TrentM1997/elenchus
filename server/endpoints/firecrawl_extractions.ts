315dad693d6e5cc4ce57f37d7ab9f133504162e7
Trent Irvin <t.m.irvin1997@gmail.com>
2025-10-26 17:55:19 -0500

    one more attempt to path env issues in heroku pipeline

--- server/endpoints/firecrawl_extractions.ts
+++ server/endpoints/firecrawl_extractions.ts
@@ -9,16 +9,18 @@ import Firecrawl from '@mendable/firecrawl-js';
 type FcResObj = { retrieved: ScrapedArticle[] | null, rejected: FailedAttempt[] };
 
 
-export function createFirecrawlClient() {
-    const key = FIRECRAWL_KEY;
+export function createFirecrawlClient(): Firecrawl | null {
+    let firecrawl
+    try {
+        firecrawl = new Firecrawl({ apiKey: process.env.FIRECRAWL_KEY })
 
-    if (!key) {
-        // Don't throw globally. Throw here so the caller can decide how to respond.
-        throw new Error("Missing FIRECRAWL_KEY in this runtime environment");
-    }
-
-    return new Firecrawl({ apiKey: key });
-}
+        return firecrawl
+    } catch (err) {
+        console.error(err);
+        firecrawl = null;
+    };
+    return firecrawl;
+};
 
 
 export const firecrawl_extractions = async (req: Request, res: Response): Promise<void> => {
@@ -26,20 +28,23 @@ export const firecrawl_extractions = async (req: Request, res: Response): Promis
     const failed: FailedAttempt[] = [];
     const { articles } = req.body;
 
-
     if ((!articles) || (!Array.isArray(articles))) {
         res.status(400).json({ error: "No articles recieved to scrape" });
         return;
     };
 
-    const key = createFirecrawlClient()
-
     try {
         const firecrawl = createFirecrawlClient();
-        const jobs: ScrapedArticle[] = await firecrawlBatchScrape(firecrawl, articles, failed);
-        const responseObj: FcResObj = { retrieved: jobs, rejected: failed };
-        res.status(200).json(responseObj);
-        return;
+        if (firecrawl) {
+            const jobs: ScrapedArticle[] = await firecrawlBatchScrape(firecrawl, articles, failed);
+            const responseObj: FcResObj = { retrieved: jobs, rejected: failed };
+            res.status(200).json(responseObj);
+            return;
+        } else {
+            res.status(401).send("Couldn't connect to firecrawl");
+            return;
+        };
+
 
     } catch (error) {
         console.error(error);
