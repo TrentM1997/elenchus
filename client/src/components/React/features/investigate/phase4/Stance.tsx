0b3b4b38af39bdc5fbcdd66b63dd2d77b5e66086
Trent Irvin <t.m.irvin1997@gmail.com>
2025-10-30 13:47:01 -0500

    small refactor on investigation wrapup component

--- client/src/components/React/features/investigate/phase4/Stance.tsx
+++ client/src/components/React/features/investigate/phase4/Stance.tsx
@@ -1,17 +1,53 @@
 import StepsEditor from "../../../Shared/TipTap/StepsEditor"
 import EndInvestigateButton from "./buttons/FinishInvestigation"
 import { getTakeAways } from "@/ReduxToolKit/Reducers/Investigate/Review"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { RootState } from "@/ReduxToolKit/store"
+import { SetStateAction, useEffect, useRef, useState } from "react";
+import type { AppDispatch } from "@/ReduxToolKit/store"
+import { displayWrapUp, displayCompletion } from "@/ReduxToolKit/Reducers/Investigate/DisplayReducer"
 
-export default function Stance({ }) {
-    const investigateState = useSelector((state: RootState) => state.investigation)
-    const { review } = investigateState
-    const { takeAway, newPOV } = review
+type Opt = 'initial' | 'Opt-in' | 'Opt-out';
+
+export default function Stance() {
+    const takeAway = useSelector((state: RootState) => state.investigation.review.takeAway);
+    const newPOV = useSelector((state: RootState) => state.investigation.review.newPOV);
+    const [option, setOption] = useState<Opt>('initial');
+    const dispatch = useDispatch<AppDispatch>();
+
+    useEffect(() => {
+        if (option === 'initial') return;
+
+        if (option === 'Opt-out') {
+
+            dispatch(displayWrapUp(false))
+            dispatch(displayCompletion(true))
+        };
+
+    }, [option, dispatch]);
+
 
     return (
         <section
             className="w-full h-full xs:px-6 flex flex-col gap-y-1 items-center content-center mx-auto">
+            {(option === 'initial') && <OptionFortakeaway key={'options'} setOption={setOption} />}
+            {(option === 'Opt-in') && <OptionalTakeaway key={'input-takeaway'} getTakeAways={getTakeAways} newPOV={newPOV} takeAway={takeAway} />}
+            {(option === 'Opt-in') && <EndInvestigateButton key={'endbutton'} />}
+        </section>
+    )
+};
+
+
+interface OptionalTakeawayProps {
+    newPOV: any,
+    takeAway: string | null,
+    getTakeAways: any
+}
+
+function OptionalTakeaway({ newPOV, takeAway, getTakeAways }: OptionalTakeawayProps): JSX.Element | null {
+
+    return (
+        <>
             <header className="xs:w-full border-b border-white/10 md:mb-2 flex justify-start">
                 <div className="w-full h-auto">
                     <h1 className="text-sm w-full lg:text-lg lg:mb-2 2xl:text-xl text-white font-light tracking-tight">
@@ -24,8 +60,40 @@ export default function Stance({ }) {
                     <StepsEditor context={takeAway} setterFunction={getTakeAways} />
                 </div>
             </main>
+        </>
+    )
+};
 
-            <EndInvestigateButton />
-        </section>
+
+interface OptionButtons {
+    setOption: React.Dispatch<SetStateAction<string>>
+};
+
+function OptionFortakeaway({ setOption }: OptionButtons): JSX.Element | null {
+
+
+    return (
+        <div className="w-full h-full flex flex-col justify-start items-center gap-8">
+            <header className="w-auto h-auto">
+                <h1 className="text-white font-light w-full tracking-tight text-sm 2xl:text-lg ">
+                    Want to write down any takeways?
+                </h1>
+            </header>
+            <div
+                className="w-auto flex items-center gap-x-2">
+                <button onClick={() => setOption("Opt-in")} className="flex items-center relative px-2 text-black hover:text-white
+                 xs:w-28 xs:h-9 lg:w-32 lg:h-12 2xl:p-2 2xl:h-12 2xl:w-36 cursor-pointer bg-white hover:bg-white/5 
+                rounded-lg transition-all duration-200 ease-inout group text-center
+                 ">
+                    Yes
+                </button>
+                <button onClick={() => setOption("Opt-out")} className="flex items-center relative px-2 text-black hover:text-white
+                 xs:w-28 xs:h-9 lg:w-32 lg:h-12 2xl:p-2 2xl:h-12 2xl:w-36 cursor-pointer bg-white hover:bg-white/5 
+                rounded-lg transition-all duration-200 ease-inout group text-center
+                 ">
+                    No
+                </button>
+            </div>
+        </div>
     )
 }
