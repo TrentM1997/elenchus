bf9099accac9439138a2208fb24dcc99e975de6c
Trent Irvin <t.m.irvin1997@gmail.com>
2026-09-06 12:31:37 -0500

    chore: stop tracking server build output

--- server/dist/core/auth/clearAuthCookies.js
+++
@@ -1,7 +0,0 @@
-async function clearAuthCookies(res) {
-    res.clearCookie('sb-access-token');
-    res.clearCookie('sb-refresh-token');
-}
-;
-export { clearAuthCookies };
-//# sourceMappingURL=clearAuthCookies.js.map
--- server/dist/helpers/getPromiseValues.js
+++
@@ -1,9 +0,0 @@
-export const getPromiseValues = (results) => {
-    const returnValues = results.map((result) => {
-        const resultData = result.value ? result.value : result.reason;
-        return resultData;
-    });
-    const success = returnValues.filter((result) => result !== undefined);
-    return success;
-};
-//# sourceMappingURL=getPromiseValues.js.map
